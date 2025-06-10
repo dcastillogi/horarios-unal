28a53bed4ab03ee299015091e91d4d559aebb17f
Daniel Castillo <dcastillogi@outlook.com>
2025-06-09 23:53:14 -0500

    update

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-
 from src.buscador import Buscador
 from src.pdf import PDF
 import json
 
-
-GITHUB = "https://github.com/danielcgiraldo"
+GITHUB = "https://github.com/dcastillogi"
 
 """
 INFORMACIÓN BUSCADOR DE CURSO
 Colocar cada campo de acuerdo al buscador de curso.
 """
 SEDE = "1102 SEDE MEDELLÍN"
-FACULTAD = "3065 FACULTAD DE CIENCIAS"
-PLAN_DE_ESTUDIOS = "3505 INGENIERÍA BIOLÓGICA"
-"""
-3068 FACULTAD DE MINAS
-    3534 INGENIERÍA DE SISTEMAS E INFORMÁTICA
-
-3065 FACULTAD DE CIENCIAS
-    3505 INGENIERÍA BIOLÓGICA
-    3647 CIENCIAS DE LA COMPUTACIÓN
-"""
+FACULTAD = "3068 FACULTAD DE MINAS"
+PLAN_DE_ESTUDIOS = "3534 INGENIERÍA DE SISTEMAS E INFORMÁTICA"
 
 def main():
 
-    # Get courses
-    print("\nCargando Horarios...")
     scraper = Buscador(options={ "campus": SEDE, "faculty": FACULTAD, "plan": PLAN_DE_ESTUDIOS })
 
     with open("src/courses.json", "w") as file:
